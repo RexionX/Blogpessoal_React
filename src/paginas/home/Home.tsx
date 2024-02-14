97dfdf95b3d488338cb69ee65d29834c5077a3fd
Jhonnattan Crescencio <jhonnattan.cres10@gmail.com>
2024-02-14 14:48:32 -0300

    Atividade React 14-02-24

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+function Home() {
+    return (
+    <>
+    <h1 className="titulo">Manipulando o mundo</h1>
+    < img className="img"src="https://kanto.legiaodosherois.com.br/w728-h381-gnw-cfill-gcc/wp-content/uploads/2022/01/legiao_g5IVuNqbsjQe.jpg.webp"
+    alt="Imagem do Eren" />
+    </>
+    );
+}
+
+export default Home;
