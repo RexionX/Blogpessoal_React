6971921021dea1e26f12fb9b3fb452bc6a59467f
Jhonnattan Crescencio <jhonnattan.cres10@gmail.com>
2024-02-25 14:53:09 -0300

    Atividade Tema

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import Cadastro from './paginas/cadastro/Cadastro';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 
 function App() {
@@ -19,11 +22,16 @@ function App() {
         <Navbar/>
         <div className="min-h-[80vh]">
           <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/" element={<Cadastro/>} />
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/home" element={<Home />}/>
+          <Route path="/temas" element={<ListaTemas />} />
+          <Route path="/cadastroTema" element={<FormularioTema />} />
+          <Route path="/editarTema/:id" element={<FormularioTema />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
           </Routes>
         </div>
         <Footer/>
